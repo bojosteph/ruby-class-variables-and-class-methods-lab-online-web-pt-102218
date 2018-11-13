681ecaa1b9ee3528031b55ea12cc80f326ff9038
Roberto Samia <bojosteph@gmail.com>
2018-11-13 01:59:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,48 @@
-class Song
-  
-  attr_accessor :name, :artist, :genre
-  
+class Song 
   @@count = 0
+  
+  def self.count 
+    @@count
+  end 
+  
+  # Control for duplicates when you code your 
+  # .genres class method, not when you add 
+  # genres to the original @@genres array.
   @@genres = []
+  
+# this should be deleted at some point
+# def self.total_genre_entries
+#   @@genres
+# end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  
+  
+  # This is how the method is done on learn.co:
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
   @@artists = []
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
   
   def initialize(name, artist, genre)
     @name = name
@@ -12,47 +50,10 @@ class Song
     @@artists << artist
     @genre = genre
     @@genres << genre
-    @@count += l
+    @@count += 1
   end
-  
-  def self.count
-    @@count
-  end
-  
-  def self.genre
-    @@genres.uniq
-  end
-  
-  def self.artists
-    @@artists.uniq
-  end
-    
-  def self.genre_count
-    genre_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1
-    else
-      genre_count[genre] = 1
-    end
-  end
-  genre_count
-end
-
-  
-  def self.artist_count
-    artist_count = {}
-  @@artists.each do |artist|
-    if artist_count[artist]
-      artist_count[artist] += 1
-    else
-      artist_count[artist] = 1
-    end
-  end
-  artist_count
-end
-
-end
+  attr_accessor :name, :artist, :genre
+end 
 
   
   
